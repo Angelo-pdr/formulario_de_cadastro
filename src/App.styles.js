8c655536591093d272863157c140cf545a356fba
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-04-27 20:42:51 -0300

    estilizado o body

---
+++ src/App.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const Body = styled.body`
+    background-color: #1C1F26;
+    height: 100vh;
+    width: 100vw;
+`
+
+export const Container = styled.div`
+    display:flex;
+    align-items: center;
+    justify-content: center;
+   
+`
