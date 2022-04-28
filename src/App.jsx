6e3e9dfb2efb308a7f9ba30f6ef3eaa09bb07345
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-04-27 21:20:03 -0300

    estilizando formulario

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ function App() {
   return (
     <C.Body>
       <C.Container>
-
+        <C.HeaderText>Registro</C.HeaderText>
+        <C.Paragrafo>Registre-se agora no <span className='servido'>seu servido</span></C.Paragrafo>
+        
+          <input type="text" placeholder='Usúario'/>
+          <input type="email" placeholder='E-mail' />
+          <input type="password" placeholder='Senha'/>
+          <input type="password" placeholder='Confirmar senha'/>
+          <input type="submit" value="Registrar-se" className='submit' />
       </C.Container>
     </C.Body>
   )
--- src/App.styles.js
+++ src/App.styles.js
@@ -5,11 +5,46 @@ export const Body = styled.body`
     background-color: #1C1F26;
     height: 100vh;
     width: 100vw;
-`
-
-export const Container = styled.div`
     display:flex;
     align-items: center;
     justify-content: center;
+    font-family: arial;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+`
+
+export const Container = styled.div`
+    height: 400px;
+    width: 350px;
+    background-color: #333333;
+    color: white;
+    border-radius: 10px;
+    padding: 15px;
+    text-align: center;
    
+    input{
+        margin: 5px 0;
+        font-size: 16px;
+        outline: none;
+        padding: 5px;
+        width: 70%;
+     
+    }
+    
+    .submit{
+        width: 50%;
+    }
+`
+export const HeaderText = styled.h1`
+    text-align: center;
+`
+
+export const Paragrafo = styled.p`
+    text-align: center;
+    font-size: 20px;
+
+    .servido{
+        color: #673AB7;
+    }
 `
