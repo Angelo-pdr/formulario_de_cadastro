b08becf30e578bd6cf2cb64a00890007c5008a6d
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-05-06 12:45:15 -0300

    estliziando pagina home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,17 +1,10 @@
 import * as C from './styles'
-import {InputPassword} from '../../components/InputPassword'
-import {InputUser} from '../../components/InputUser'
-
 
 export const Home = () => {
 
     return(
         <C.Container>
-        <C.HeaderText>Registro</C.HeaderText>
-        <C.Paragrafo>Registre-se agora no <span className='servido'>seu servido</span></C.Paragrafo>
-            <InputUser /> 
-            <InputPassword />
-          <input type="submit" value="Registrar-se" className='submit' />
-      </C.Container>
+       
+        </C.Container>
     )
 }
--- src/routes/mainRoutes.jsx
+++ src/routes/mainRoutes.jsx
@@ -1,9 +1,10 @@
 import {useRoutes} from 'react-router-dom'
 import {Home} from '../Pages/Home'
+import {Register} from '../Pages/Register'
 
 export const MainRoutes = () =>{
     return useRoutes([
         {path: '/', element:<Home />},
-        
+        {path: '/cadastro', element:<Register />}
     ])
 }
