ef67f316363672ecef90fe80a9ce596b70929125
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-05-12 20:34:55 -0300

    fazendo function para redireciona o usuario para pagina principal

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,16 +1,20 @@
 import * as C from './styles'
 import {InputUser} from '../../components/HomeComponents'
-import {Link} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 import {useState} from 'react'
 
 
+
 export const Home = () => {
+    
+    const navigate = useNavigate()
     const [userList, setUserList] = useState (JSON.parse(localStorage.getItem('user') || '[]'))
 
-    const validUser = (taskPassword, taskEmail) =>{
+    const validUser = (taskPassword, taskEmail, event) =>{
         for(let index in userList){
-            if(taskPassword == userList[index].password &&  taskEmail === userList[index].email ){
-                
+            if(taskPassword == userList[index].password && taskEmail == userList[index].email){
+                console.log(event)
+                navigate("/dashboard")
             }
         }
     }
@@ -19,7 +23,7 @@ export const Home = () => {
         <C.Container>
             <C.HeaderText>Login</C.HeaderText>
             <InputUser validUser={validUser}/>
-            <input type="submit" value="loga" className='submit'  onClick={validUser}/>
+            <input type="submit" value="loga" className='submit' onClick={validUser}/>
             <C.P><Link to="/cadastro" className='link'>Criar uma conta!</Link></C.P>
         </C.Container>
     )
--- src/Pages/dashboard/index.jsx
+++ src/Pages/dashboard/index.jsx
@@ -1,12 +1,10 @@
 import * as C from './styles'
 
-
 export const Dashboard = () => {
 
-    
     return(
         <C.Container>
-            <C.Text>Bem Vindo </C.Text>
+            <C.Text>Bem Vindo</C.Text>
         </C.Container>
     )
 }
